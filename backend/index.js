6f3cd8681827af107e609b825d14d8cf3720ceb0
Manuel Castillo <mac.jmanuel@gmail.com>
2020-11-19 14:53:29 -0600

    'morgan'

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const morgan = require('morgan')
 const express = require('express')
 const app = express()
 
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
