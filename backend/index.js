a43f04f1744695ca8ddd52fab23ffccfc34e6014
Angel8778998 <angurie_2008@hotmail.com>
2020-11-19 10:16:32 -0600

    adding server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-//manu se la come
+const morgan = require('morgan')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('server is running');
+})
