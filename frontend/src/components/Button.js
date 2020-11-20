6d7c6e2c4a18f31b3c0ad71b1cc3f5f0a6a8f9f8
Angel8778998 <angurie_2008@hotmail.com>
2020-11-20 15:21:22 -0600

    Adding Buttons

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+import '../assets/styles/components/Button.css'
+
+const Button = ({type, size, text, onClick}) => {
+    return (
+        <>
+        <button className={`button 
+            ${type === 'primary' ? "primary" : "secundary"} 
+            ${size === 'medium' ? "medium" : "large"}`}
+            onClick={onClick}
+        >
+        {text}
+        </button>
+        </>
+    )
+}  
+const options={
+    types:['primary', 'secundary'],
+    sizes:['medium','large']
+}
+
+Button.propTypes = {
+    onClick: PropTypes.func,
+    type: PropTypes.oneOf(options.types),
+    size: PropTypes.oneOf(options.sizes),
+    text: PropTypes.string
+}
+Button.defaultProps = {
+    type: 'primary',
+    size: 'large',
+    onClick: () => {},
+    text: 'default'
+}
+
+export default Button;
---
+++ frontend/src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../components/Button'
+
+const App = () => {
+    return (
+        <>
+        <Button type={'primary'} size={'large'} />
+        <Button type={'secundary'} size={'medium'} />
+        <Button type={'primary'} size={'medium'} />
+        </>
+    )
+}
+
+export default App;
