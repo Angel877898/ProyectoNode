8f88ad393204c18811057f62ab44348c31000409
Manuel Castillo <mac.jmanuel@gmail.com>
2020-11-25 16:32:54 -0600

    *changed* base de datos actualizada

--- backend/empleadosAct.sql
+++ backend/empleadosAct.sql
@@ -46,6 +46,29 @@ INSERT INTO `admins` (`id`, `nombre`, `apellidos`, `telefono`, `correo`, `direcc
 (2, 'Angel', 'Hernandez', '4426581548', 'angel@mail.com', 'Ignacio Zaragoza, Col. Rancho Largo', '123456'),
 (3, 'Moisés', 'Pérez', '4429995564', 'moises@mail.com', 'Ignacio Zaragoza, Col. Rancho Largo', '123456');
 
+
+INSERT INTO `empleados` (`id`, `nombre`, `apellidos`, `telefono`, `correo`, `direccion`) VALUES
+(1, '\r\njuan', 'Perez', '4433221100', 'juan@emp.com', 'Ocampo 1'),
+(2, 'Pedro', 'Sanchez', '3344221100', 'pedro@emp.com', 'lazaro 1'),
+(3, 'Marco', 'Rois', '6655443322', 'marco@emp.com', 'Alemania 3'),
+(4, 'Cristian', 'Rolando', '7777777777', 'cr7@emp.com', 'Portugal 7'),
+(5, 'Lio', 'Messi', '1010101010', 'lio@emp.com', 'pechofrio 10'),
+(6, 'Lukita', 'Modric', '1210101010', 'lm10@emp.com', 'Croacia 10'),
+(7, 'Antonio', 'Kruz', '8888888888', 'toni@emp.com', 'Alemania 8'),
+(8, 'Manu', 'Pingacorta', '7755332266', 'manu@emp.com', 'Edomex 10'),
+(9, 'Angel', 'Carano', '4455668822', 'angel@emp.com', 'Rio verde 10'),
+(10, 'Sergio', 'Ramos', '4444444444', 'sergio@emp.com', 'España 4'),
+(11, 'Vinicius', 'Junior', '2121212121', 'vini@emp.com', 'Brasil 21'),
+(12, 'Rodrygo', 'Goes', '1212121212', 'rodry@emp.com', 'Brasil 12'),
+(13, 'Paulo', 'Dybala', '2101010102', 'dyb@gmail.com', 'Argentina 10'),
+(14, 'Moises', 'ElChidote', '4432111588', 'moy@emp.com', 'Michoacan 7'),
+(15, 'Karim', 'Benzema', '9999999999', 'karim@emp.com', 'Francia 9'),
+(16, 'Kylian', 'Mbappe', '1010101018', 'kyliam@emp.com', 'Francia 10'),
+(17, 'Harry', 'Kane', '1091091091', 'harry@emp.com', 'Inglaterra 9'),
+(18, 'Carlos', 'Casemiro', '1414141414', 'case@emp.com', 'Brasil 14'),
+(19, 'Dani', 'Carvajal', '2222222222', 'dani@emp.com', 'España 2'),
+(20, 'Tibo', 'Curtua', '1111111111', 'tibo@emp.com', 'Belgica 1');
+
 -- --------------------------------------------------------
 
 --
