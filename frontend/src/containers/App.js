5a9e93972fa0154208672031c3b8319f1b3b619b
Manuel Castillo <mac.jmanuel@gmail.com>
2020-11-25 14:41:42 -0600

    *added* componente sidebar

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 // import Input from '../components/Input'
-import Modal from '../components/Modal'
+// import Modal from '../components/Modal'
+import Sidebar from '../components/Sidebar'
 
 const App = () => {
     return (
-        <>
-            <Modal />
+        <> 
+            <Sidebar />
         </>
     )
 }
