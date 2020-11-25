7e50f430f0998e011db70238419f27a65002c059
Manuel Castillo <mac.jmanuel@gmail.com>
2020-11-25 00:25:29 -0600

    *added* componente modal

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Input from '../components/Input'
+// import Input from '../components/Input'
+import Modal from '../components/Modal'
 
 const App = () => {
     return (
         <>
-            <Input />
+            <Modal />
         </>
     )
 }
