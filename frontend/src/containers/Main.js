ed963f5d403ecad3aa826b9fc791e319320d6f79
Manuel Castillo <mac.jmanuel@gmail.com>
2020-11-27 00:41:58 -0600

    *added* modal para agregar empleado en main

--- frontend/src/containers/Main.js
+++ frontend/src/containers/Main.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../components/NavBar'
 import Sidebar from '../components/Sidebar'
 import Input from '../components/Input'
 import CardHeader from '../components/CardHeader'
 import CardUser from '../components/CardUser'
 import Button from '../components/Button'
+import Modal from '../components/Modal'
 import '../assets/styles/containers/Main.css'
 
 const Main = () => {
+    const [{showModalState, modalType}, setShowModalState] = useState({ showModalState: false, modalType: { action: '', message: '' } })
+
+    // funcion para renderizar modal / modaType determina que modal sale
+    const handleShowAddModal = () => {
+        setShowModalState({
+            showModalState: true,
+            modalType: {
+                // tipo de modal
+                action: 'add',
+                // titulo de modal
+                message: 'Nuevo empleado'
+            }
+        })
+    }
+    // const handleShowDelModal = () => {
+    //     setShowModalState({
+    //         showModalState: true,
+    //         modalType: {
+    //             action: 'del',
+    //             message: '¿Eliminar empleado?'
+    //         }
+    //     })
+    // } 
+    // const handleShowModModal = () => {
+    //     setShowModalState({
+    //         showModalState: true,
+    //         modalType: {
+    //             action: 'mod',
+    //             message: 'Modificar empleado'
+    //         }
+    //     })
+    // } 
+
     return (
         <>
             <NavBar />
@@ -32,9 +66,11 @@ const Main = () => {
                     </div>
                     <Button 
                         text='Nuevo empleado'
+                        onClick={ handleShowAddModal }
                     />
                 </div>                
             </div>
+            { showModalState && <Modal type={ modalType }/>}
         </>
     )
 }
