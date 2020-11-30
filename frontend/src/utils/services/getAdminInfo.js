4ca9da025d952f2aaa1b088ed3c5910d93d0ac9a
Angel8778998 <angurie_2008@hotmail.com>
2020-11-30 17:31:11 -0600

    added gravatar and admin info

--- frontend/src/utils/services/getAdminInfo.js
+++ frontend/src/utils/services/getAdminInfo.js
@@ -2,6 +2,7 @@ import jwt_decode from "jwt-decode";
 export default function getAdminInfo() {
     const gettoken = sessionStorage.getItem('token')
     const codedToken=jwt_decode(gettoken)
+    
     return codedToken
     
 }
