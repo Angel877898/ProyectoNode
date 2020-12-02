129b2ae090810b20c1c6e94fbf2cea6021f55f3e
Manuel Castillo <mac.jmanuel@gmail.com>
2020-12-01 21:29:26 -0600

    *added* retroalimentacion en errores

--- frontend/src/components/RequestStatusSnackbar.js
+++ frontend/src/components/RequestStatusSnackbar.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import '../assets/styles/components/RequestStatusSnackbar.css'
+import { DataContext } from '../utils/DataContext'
+
+const closeIcon = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
 
 const RequestStatusSnackbar = ({ status, message }) => {
+
+    const { setSnackbarState } = useContext(DataContext)
+
     return (
         <div className={`snackbar ${ status }`}>
             <p>{ message }</p>
+            <span onClick={() => setSnackbarState({snackbarState: false})}>
+                { closeIcon }
+            </span>
         </div>
     )
 }
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -74,7 +74,6 @@ const Login = ({ history }) => {
                             type={ false }
                             onChange={handlePass}
                         />
-                        {/* TODO checkbox<p>*TO DO Checkbox*</p> */}
                         <Button type="primary" size="large" text='Iniciar Sesion' />
                     
                     </div>
